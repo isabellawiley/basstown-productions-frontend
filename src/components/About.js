8275f535c7c8fa5dc19ca9560a8295850655596d
Isabella <wileyisabella@gmail.com>
2023-09-29 13:37:26 -0400

    about page layout completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,15 @@
-
+import hero from "./images/hero-img.png";
+import '../styling/about.css';
 
 function About(){
 
     return(
         <div>
-            About
+            <div id='hero-pic-container'>
+                <img src={hero} alt="hero" className="hero-pic"/>
+                <div className="overlay">ABOUT</div>
+            </div>
+            <span className="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
         </div>
     )
 }
