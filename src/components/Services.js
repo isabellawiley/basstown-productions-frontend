5f8877672a22a2dc2f3ac43177e42ccbc54328af
Isabella <wileyisabella@gmail.com>
2023-09-29 13:59:40 -0400

    services layout completed

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,38 @@
-
+import hero from "./images/hero-img.png";
+import quote1 from "./images/quote-img-1.jpg";
+import '../styling/services.css';
 
 function Services(){
 
     return(
         <div>
-            Services
+            <div id='hero-pic-container'>
+                <img src={hero} alt="hero" className="hero-pic"/>
+                <div className="overlay">SERVICES</div>
+            </div>
+            <div className="services-container">
+                <div className="service">
+                    <img src={quote1} alt="service" className="quote-img"/>
+                    <div className="service-details">
+                        <h2>Service Title</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+                <div className="service">
+                    <img src={quote1} alt="service" className="quote-img"/>
+                    <div className="service-details">
+                        <h2>Service Title</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+                <div className="service">
+                    <img src={quote1} alt="service" className="quote-img"/>
+                    <div className="service-details">
+                        <h2>Service Title</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
