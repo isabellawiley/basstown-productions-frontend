0c01c6fb99d0ef389e2625731b6bb826610c5666
Isabella <wileyisabella@gmail.com>
2023-10-02 14:51:35 -0400

    contact page social media logos

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,21 @@
-
+import hero from "./images/hero-img.png";
+import '../styling/contact.css';
 
 function Contact(){
 
     return(
         <div>
-            Contact
+            <div id='hero-pic-container'>
+                <img src={hero} alt="hero" className="hero-pic"/>
+                <div className="overlay">CONTACT</div>
+            </div>
+            <div>
+                <div className="social-links-container">
+                    <div className="circle insta"></div>
+                    <div className="circle facebook"></div>
+                    <div className="circle linkedin"></div>
+                </div>
+            </div>
         </div>
     )
 }
